c522d89e19ada09bccaaca0fc6efcbeed7ea80d9
Huseyin <huseyin.bayazit32@gmail.com>
2025-01-28 23:50:58 +0300

    Create header.js

---
+++ scripts/js/header.js
@@ -0,0 +1,1 @@
+var expanded = false; function expand() {var elem = document.getElementById("header-content-container"); expanded = !expanded; if (expanded) {elem.style = "display: block;";} else {elem.style = "";}}
